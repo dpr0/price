5dda698d90a8c777997473ed31e50d9a52554cba
dvitvitskiy <dvitvitskiy@npoprogress.com>
2020-05-04 23:21:00 +0300

    manufacturer_ids

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,15 @@
 class ItemsController < ApplicationController
 
   def index
-    @items = params[:category_id].present? ? Item.where(category_id: params[:category_id]) : Item.all
-    @categories = Category.all
-    @manufacturers = Manufacturer.all
+    @items = params[:category_id].present? ? Item.where(category_id: params[:category_id]) : Item
+    @items = @items.all.to_a.group_by(&:category_id)
   end
 
   def filter
-    @items = Item.where(manufacturer_id: 1)
+    search = {}
+    search[:manufacturer_id] = params[:manufacturer_ids] if params[:manufacturer_ids].present?
+    search[:category_id]     = params[:category_id]      if params[:category_id].present?
+    @items = Item.where(search).all.to_a.group_by(&:category_id)
     render layout: false
   end
-
 end
