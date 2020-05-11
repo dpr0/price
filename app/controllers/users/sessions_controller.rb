c1695abd24f843bf84987867772528f20b5f57b5
dvitvitskiy <dvitvitskiy@npoprogress.com>
2020-05-11 13:35:00 +0300

    flash

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,11 +3,32 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  def new
+  def yandex
     # byebug
-    super
+    # https://oauth.yandex.ru/authorize
+    # client_id     = 1c6dd3ee9d5b4ce5baee54d2c09dcaec
+    # redirect_uri  = http://localhost:3000/users/auth/yandex/callback
+    # response_type = code
+    # state         = 16ed1c94496a6fbb8b4d0062c7dd1e874e63790e7bb9fb06
+    omniauth_provider('YANDEX')
   end
+
+  def github
+    # byebug
+    # https://github.com/login
+    # client_id     = 9a2d962a2e052c47abc4
+    # return_to     = /login/oauth/authorize
+    # client_id     = 9a2d962a2e052c47abc4
+    # redirect_uri  = http://localhost:3000/users/auth/github/callback
+    # response_type = code
+    # state         = f138b02b339a79435b34f61799508ce9d4323be06872d129
+    omniauth_provider('GITHUB')
+  end
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
 
   # POST /resource/sign_in
   def create
@@ -16,9 +37,10 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    # byebug
+    super
+  end
 
   # protected
 
@@ -26,4 +48,13 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def omniauth_provider(provider_name)
+    render json: request.env['omniauth.auth']
+    # @user = User.find_for_oauth(request.env['omniauth.auth'])
+    # if @user.persisted?
+    #   sign_in_and_redirect @user, event: :authentication
+    #   set_flash_message(:notice, :success, kind: provider_name) if is_navigational_format?
+    # end
+  end
 end
